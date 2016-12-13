7c476484df9bacf9426592e724e6592f2ea34aa9
Jean Bahnik <jeanbahnik@gmail.com>
2016-12-12 16:19:46 -0800

    ex3 and ex4

---
+++ ex3.rb
@@ -0,0 +1,76 @@
+# An intro to the exercise.
+puts "I will now count my chickens:"
+
+# Counting the number of hens.
+puts "Hens #{25 + 30 / 6}"
+# Counting the number of roosters.
+puts "Roosters #{100 - 25 * 3 % 4}"
+
+# Switching to a differnt count.
+puts "Now I will count the eggs:"
+
+# Doing some math with Ruby.
+puts 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+
+# Intro to the next section
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# Outputting the result of the math question before.
+puts 3 + 2 < 5 - 7
+
+# First part of the question.
+puts "What is 3 + 2? #{3 + 2}"
+# Second part of the question.
+puts "What is 5 - 7? #{5 - 7}"
+
+# Just a line of text.
+puts "Oh, that's why it's false."
+
+# More text.
+puts "How about some more."
+
+# Comparing with greater than.
+puts "Is is greater? #{5 > -2}"
+# Comparing with greater or equal.
+puts "Is it greater or equal? #{5 >= -2}"
+# Comparing with less or equal.
+puts "Is it less or equal? #{5 <= -2}"
+
+puts "Using floats."
+# An intro to the exercise.
+puts "I will now count my chickens:"
+
+# Counting the number of hens.
+puts "Hens #{25.0 + 30.0 / 6.0}"
+# Counting the number of roosters.
+puts "Roosters #{100.0 - 25.0 * 3.0 % 4.0}"
+
+# Switching to a differnt count.
+puts "Now I will count the eggs:"
+
+# Doing some math with Ruby.
+puts 3.0 + 2.0 + 1.0 - 5.0 + 4.0 % 2.0 - 1.0 / 4.0 + 6.0
+
+# Intro to the next section
+puts "Is it true that 3 + 2 < 5 - 7?"
+
+# Outputting the result of the math question before.
+puts 3.0 + 2.0 < 5.0 - 7.0
+
+# First part of the question.
+puts "What is 3 + 2? #{3.0 + 2.0}"
+# Second part of the question.
+puts "What is 5 - 7? #{5.0 - 7.0}"
+
+# Just a line of text.
+puts "Oh, that's why it's false."
+
+# More text.
+puts "How about some more."
+
+# Comparing with greater than.
+puts "Is is greater? #{5.0 > -2.0}"
+# Comparing with greater or equal.
+puts "Is it greater or equal? #{5.0 >= -2.0}"
+# Comparing with less or equal.
+puts "Is it less or equal? #{5.0 <= -2.0}"
