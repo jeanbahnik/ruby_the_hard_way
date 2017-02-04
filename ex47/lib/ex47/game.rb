f19a5f982fe3363ade86f8ef572c05759a6194d6
Jean Bahnik <jeanbahnik@gmail.com>
2017-02-03 21:49:58 -0800

    47/52

---
+++ ex47/lib/ex47/game.rb
@@ -0,0 +1,20 @@
+class Room
+
+  def initialize(name, description)
+    @name = name
+    @description = description
+    @paths = {}
+  end
+
+  # thse make it easy for you to access the variables
+  attr_reader :name, :paths, :description
+
+  def go(direction)
+    return @paths[direction]
+  end
+
+  def add_paths(paths)
+    @paths.update(paths)
+  end
+
+end
